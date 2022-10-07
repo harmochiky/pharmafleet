1c959927ccaefafba5211996e9a48f70346bf1df
Harmony <harmochiky2@gmail.com>
2022-10-07 04:05:59 +0200

    Home ordering info

--- src/components/Home/HomeOrderingInfo.js
+++ src/components/Home/HomeOrderingInfo.js
@@ -3,28 +3,29 @@ import AboutPic from "../../assets/img/about.png";
 
 export default function HomeOrderingInfo() {
   return (
-    <div className="container-fluid px-5 py-4 home-order-about">
+    <div className="container px-5 py-4 home-order-about">
       <div className="row">
         <div className="col-6 px-2">
           <img src={AboutPic} alt="" className="img-fluid" />
         </div>
-        <div className="col-4 px-3 details">
+        <div className="col-6 px-3 details">
           <div>
             <h3 className="bold">
-              How to order medicines on Medilazar? It’s Simple.
+              How to order medicine ?<br />{" "}
+              <span className="text-primary">It’s simple.</span>
             </h3>
             <div className="my-4">
               <div className="point">
                 <span className="point-number">
                   <span>1</span>
                 </span>
-                <span>Upload valid Prescription</span>
+                <span>Log into your account</span>
               </div>
               <div className="point">
                 <span className="point-number">
                   <span>2</span>
                 </span>
-                <span>Receive a confirmation call</span>
+                <span>Select products that you want and checkout</span>
               </div>
               <div className="point">
                 <span className="point-number">
@@ -33,12 +34,12 @@ export default function HomeOrderingInfo() {
                 <span>Delivery at your pharmacy</span>
               </div>
             </div>
-            <h5 className="bold">Don't have Prescription ?</h5>
-            <div className="text-secondary">
-              Simply search &amp; add the medicines OR get consultation from
-              Medilazar doctor
+            <h6 className="bold">Don't have account ?</h6>
+            <div className="text-secondary w-75">
+              Simply press the button below and sign up then wait for us to
+              approve your pharmacy
             </div>
-            <div className="mt-3">Apply here</div>
+            <div className="mt-3 btn btn-primary rounded-pill">Apply here</div>
           </div>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,7 @@ export default function Home() {
     <section>
       <HomeSlider />
       <Services />
+      <HomeOrderingInfo />
       <DealofDay />
       <Banner2 />
       <ProductCardSlider
